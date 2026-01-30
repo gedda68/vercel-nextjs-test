7800b9f72d86770a3f919eb5cf91e771e7f47a40
gedda <gaitchis@gmail.com>
2026-01-31 08:03:34 +1000

    my first vercel page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>Hello Vercel 👋</h1>
+      <p>My first deployed page.</p>
+    </main>
+  );
+}
